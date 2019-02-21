7cd9bc865655f2e75393561617d90c95a2dd3a52
HatemHassan-xo <hatem@crazyegg.com>
2019-02-21 06:06:04 +0100

    first commit

---
+++ public/draw.js
@@ -0,0 +1,194 @@
+function drawLine() {
+  console.log("==> drawLine()");
+
+  xPos = width / 2;
+  // draw vertical timeline
+  mainSVG
+    .append("line")
+    .attr("transform", "translate(" + CONFIG.margin.left + "," + CONFIG.margin.top + ")")
+
+    .attr("stroke", CONFIG.colors["dark-blue"])
+    .attr("stroke-width", 4)
+
+    .attr("x1", xPos)
+    .attr("y1", 0)
+    .attr("x2", xPos)
+    .attr("y2", 0)
+
+    .transition()
+    .attr("y2", height)
+    .duration(2000)
+    .ease(d3.easeBackOut)
+    .on("end", () => {
+      addEarthquakes();
+      showEarthquake(0);
+    });
+}
+
+
+function addEarthquakes() {
+  console.log("==> addDataPoints()");
+
+
+  let elem = mainSVG
+    .append("g")
+    .attr("id", "earthquakes")
+    .attr("transform", "translate(" + CONFIG.margin.left + "," + CONFIG.margin.top + ")")
+
+    .selectAll("g")
+    .data(data);
+
+  // Init Groups
+  earthquakes = elem.enter()
+    .append("g")
+    .attr("id", (d) => "e" + d.eventid)
+    .classed("hide", true)
+
+
+  // Add circles
+  earthquakes
+    .append("circle")
+    .attr("cy", height / 4 * 3)
+    .attr("cx", width / 2)
+    .attr("r", 0)
+    .attr("fill", CONFIG.colors["green"])
+
+    .transition()
+    .attr("r", CONFIG.circleRadius)
+    .duration(500)
+    .ease(d3.easeBackOut);
+
+
+  // Add Country
+  earthquakes
+    .append("text")
+    .attr("y", (height / 4 * 3) + 5)
+    .attr("x", width / 2)
+    .style("text-anchor", "middle")
+    .text((d) => d.country);
+
+  // Add date
+  earthquakes
+    .append("text")
+    .classed("date", true)
+    .attr("y", (height / 4 * 3) + 25)
+    .attr("x", (width / 2))
+    .style("text-anchor", "middle")
+    .text((d) => d.datetime);
+
+  // Add Magnitude
+  earthquakes
+    .append("text")
+    .classed("mag", true)
+    .attr("y", (height / 4 * 3) + 5)
+    .attr("x", (width / 2) + 40)
+    .style("text-anchor", "left")
+    .text((d) => d.magnitude + " unit?!!");
+
+  // Add Magnitude line
+  earthquakes
+    .append("line")
+    .classed("mag", true)
+    .attr("y1", (height / 4 * 3) + 15)
+    .attr("x1", (width / 2) + 40)
+    .attr("y2", (height / 4 * 3) + 15)
+    .attr("x2", (width / 2) + 40)
+    .attr("stroke", CONFIG.colors["pinkish"])
+    .attr("stroke-width", 4)
+
+  // Add Deaths
+  earthquakes
+    .append("text")
+    .classed("deaths", true)
+    .attr("y", (height / 4 * 3) + 5)
+    .attr("x", (width / 2) - 80)
+    .style("text-anchor", "left")
+    .text((d) => d.magnitude + " ppl died :(");
+
+  // Add Deaths line
+  earthquakes
+    .append("line")
+    .classed("deaths", true)
+    .attr("y1", (height / 4 * 3) + 15)
+    .attr("x1", (width / 2) - 40)
+    .attr("y2", (height / 4 * 3) + 15)
+    .attr("x2", (width / 2) - 40)
+    .attr("stroke", CONFIG.colors["pinkish"])
+    .attr("stroke-width", 4)
+
+}
+
+function showEarthquake(i) {
+  if (data[i]) {
+    console.log("==> showEarthquake()", i);
+    d3.select("#e" + data[i].eventid)
+      .classed("hide", false);
+
+    d3.select("#e" + data[i].eventid + " line.mag")
+      .transition()
+      .attr("x2", (d) => (width / 2) + xMagScale(d.magnitude))
+      .duration(CONFIG.delays.stackDelay)
+      .ease();
+
+    d3.select("#e" + data[i].eventid + " text.mag")
+      .transition()
+      .attr("x", (d) => (width / 2) + 200)
+      .duration(CONFIG.delays.stackDelay)
+      .ease();
+    
+     d3.select("#e" + data[i].eventid + " line.deaths")
+       .transition()
+       .attr("x2", (d) => (width / 2) - xMagScale(d.magnitude))
+       .duration(CONFIG.delays.stackDelay)
+       .ease();
+
+     d3.select("#e" + data[i].eventid + " text.deaths")
+       .transition()
+       .attr("x", (d) => (width / 2) - 200)
+       .duration(CONFIG.delays.stackDelay)
+       .ease();
+  }
+
+}
+
+function stackEarthquake(i) {
+  if (data[i]) {
+    console.log("==> stackEarthquake()", i);
+    d3.select("#e" + data[i].eventid + " circle")
+      .transition()
+      .attr("cy", yPositionScale(i))
+      .attr("r", 20)
+      .duration(CONFIG.delays.stackDelay)
+      .ease();
+
+    d3.selectAll("#e" + data[i].eventid + " text:not(.date)")
+      .transition()
+      .attr("y", yPositionScale(i) + 5)
+      .style("font-size", 10)
+      .duration(CONFIG.delays.stackDelay)
+      .ease();
+
+    d3.selectAll("#e" + data[i].eventid + " .date")
+      .transition()
+      .attr("y", yPositionScale(i) + 25)
+      .style("font-size", 10)
+      .duration(CONFIG.delays.stackDelay)
+      .ease();
+
+    d3.selectAll("#e" + data[i].eventid + " line.mag")
+      .transition()
+      .attr("y1", yPositionScale(i) + 15)
+      .attr("y2", yPositionScale(i) + 15)
+      .style("font-size", 10)
+      .duration(CONFIG.delays.stackDelay)
+      .ease();
+
+    d3.selectAll("#e" + data[i].eventid + " line.deaths")
+      .transition()
+      .attr("y1", yPositionScale(i) + 15)
+      .attr("y2", yPositionScale(i) + 15)
+      .style("font-size", 10)
+      .duration(CONFIG.delays.stackDelay)
+      .ease();
+  }
+}
---
+++ public/main.js
@@ -0,0 +1,96 @@
+const CONFIG = {
+  circleRadius: 30,
+  mainSVGwidth: 900,
+  mainSVGheight: 600,
+  margin: {
+    top: 20,
+    right: 20,
+    bottom: 20,
+    left: 20
+  },
+
+  colors: {
+    "main": "#F0E5D6",
+    "pinkish": "#F28C81",
+    "green": "#8AC6BF",
+    "teal": "#7C96A8",
+    "dark-blue": "#01395C"
+  },
+
+  delays: {
+    stackDelay: 1000
+  }
+};
+
+let height = CONFIG.mainSVGheight - CONFIG.margin.top - CONFIG.margin.bottom;
+let width = CONFIG.mainSVGwidth - CONFIG.margin.left - CONFIG.margin.right;
+let mainSVG, earthquakes = null;
+
+let yPositionScale, xMagScale = null;
+
+let nextElem = 0;
+
+
+window.onload = () => {
+  // Main Viz
+  d3.select("#main")
+    .style("max-width", `${CONFIG.width}px`)
+    .style("margin", "0 auto")
+    .append("svg")
+    .attr("id", "mainSVG")
+    .attr("width", width + CONFIG.margin.left + CONFIG.margin.right)
+    .attr("height", height + CONFIG.margin.top + CONFIG.margin.bottom)
+
+  mainSVG = d3.select("#mainSVG");
+
+  console.log("==> Initialized");
+  parseData(drawLine);
+
+  document.onkeydown = (event) => {
+    if (event.keyCode == '40') {
+      next();
+    }
+  };
+};
+
+
+function parseData(drawFn) {
+  console.log("==> parseData()");
+  d3.json("earthquakes.json").then((d) => {
+    data = d;
+    console.log("==> imported", data);
+
+    configureScales();
+    drawFn();
+  });
+}
+
+function configureScales() {
+  yPositionScale = d3.scaleLinear()
+    .domain([0, data.length])
+    .range([CONFIG.margin.top, height / 5 * 4]);
+  
+  
+  xMagScale = d3.scaleLinear()
+    .domain([0, d3.max(getProperty("magnitude"))])
+    .range([0, width/2]);
+}
+
+
+
+function next() {
+  stackEarthquake(nextElem);
+  nextElem++;
+  setTimeout(() => {
+    showEarthquake(nextElem);
+  }, CONFIG.delays.stackDelay/2);
+
+}
+
+function getProperty(property) {
+  return data.map((dataPoint) => dataPoint[property]);
+}
+
+function reset() {
+  location.reload();
+}
